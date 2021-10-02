444a1ca7a533d12bebb8cee90e9ce2d91ef23138
sungyun <sungyun5423@gmail.com>
2021-10-03 00:07:59 +0900

    Prettier 적용

--- Lv1/수박수박수박수박수박수.ts
+++ Lv1/수박수박수박수박수박수.ts
@@ -1,3 +1,3 @@
-export const solution = (n: number) => "수박".repeat(n).substring(0, n);
+export const solution = (n: number) => '수박'.repeat(n).substring(0, n);
 
 // https://programmers.co.kr/learn/courses/30/lessons/12922
--- Lv1/짝수와홀수.ts
+++ Lv1/짝수와홀수.ts
@@ -1,3 +1,3 @@
-export const solution = (num: number) => (num % 2 === 0 ? "Even" : "Odd");
+export const solution = (num: number) => (num % 2 === 0 ? 'Even' : 'Odd');
 
 // https://programmers.co.kr/learn/courses/30/lessons/12937
